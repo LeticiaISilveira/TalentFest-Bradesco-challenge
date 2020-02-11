abbccc1251701e90c92486f7ea2dc47276ee0ae9
Leticia <leticia.ingrid.silveira@gmail.com>
2020-02-11 15:35:57 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Select from './components/select'
 
 function App() {
   return (
     <div className="App">
+      <Select country={'banana e peixinho'}/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
