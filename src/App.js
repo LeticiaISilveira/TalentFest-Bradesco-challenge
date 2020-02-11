3530a1585eb8e45d9ba2f26b9cc02d99e5eefa45
rosi-souza <rosi.souza@live.com>
2020-02-11 16:19:13 -0300

    criando componente home

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import Card from './components/card'
+import React from 'react';
+import Home from './components/home';
 
 function App() {
-  const [dados, setDados] = useState([]);
-
-let dadosApi = "https://economia.awesomeapi.com.br/all/";
-useEffect(() => {
-  fetch(dadosApi)
-    .then(res => res.json())
-    .then(res => setDados(res))
-  }, []);
-  
-
-  const values = Object.values(dados)
-  console.log(values.map((item) => item))
   return (
-    <div className="App">
-    {values.map((item) =>
-          <ul>
-          <li>{item.code}</li>
-          <span>{item.name}</span>
-          <span>{item.high}</span>
-        </ul>
-    )}
-
+    <div>
+     <Home />
     </div>
   );
 }
